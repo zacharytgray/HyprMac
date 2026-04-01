a2f5851d4d8c0d8a2af34ca699caa1439ce7fc52
Zachary Gray <zacharytgray@gmail.com>
2026-04-01 13:34:40 -0500

    Release v0.1.0

--- Casks/hyprmac.rb
+++ Casks/hyprmac.rb
@@ -1,6 +1,6 @@
 cask "hyprmac" do
   version "0.1.0"
-  sha256 "PLACEHOLDER"
+  sha256 "ec4e695026ba445b2f1a38c6b9e38e1acef7b8227f7cd55e06b443d2842f4283"
 
   url "https://github.com/zacharytgray/HyprMac/releases/download/v#{version}/HyprMac-#{version}.dmg"
   name "HyprMac"
